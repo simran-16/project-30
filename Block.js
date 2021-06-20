8f33e07c29b2de06ace5f323f1e92186a3e5a4bf
simran-16 <77108358+simran-16@users.noreply.github.com>
2021-06-20 13:50:20 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,37 @@
+class Block{
+  constructor(x, y, width, height) {
+    
+      var options = {
+        
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image= loadImage("block.png");
+      this.Visibility= 255;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      console.log(this.body.speed);
+    if(this.body.speed<3){
+
+    
+    
+    }
+    else{
+      World .remove(world,this.body);
+      push ();
+      this.Visibility=this.Visibility-5;
+      tint (255,this.Visibility);
+      image(this.image,this.body.position.x,this.body.position.y,50,50);
+      pop ();   
+    }
+
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+    
+}
+}
